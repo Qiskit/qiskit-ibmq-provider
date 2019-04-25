6f1498233e61aca1b2ed9f6c2076a3df47c81ef8
Diego M. Rodriguez <diego.plan9@gmail.com>
2019-04-25 19:39:07 +0200

    Add api.session.RetrySession handler
    Add `RetrySession` as the object that encapsulates the settings and
    handling of http-related communication, replacing part of the `utils.`
    `Credentials` and `Request` classes via subclassing `request.Session`
    with additional functionality (retrying, auto appending headers, get
    parameters and url-mangling).

---
+++ qiskit/providers/ibmq/api/session.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2018, IBM.
+#
+# This source code is licensed under the Apache License, Version 2.0 found in
+# the LICENSE.txt file in the root directory of this source tree.
+
+"""Session customized for IBM Q access."""
+
+from requests import Session
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
+STATUS_FORCELIST = (
+    500,  # Internal Server Error
+    502,  # Bad Gateway
+    503,  # Service Unavailable
+    504,  # Gateway Timeout
+)
+CLIENT_APPLICATION = 'qiskit-api-py'
+
+
+class RetrySession(Session):
+    """Session with retry and handling of IBM Q parameters."""
+    
+    def __init__(self, base_url, access_token=None,
+                 retries=5, backoff_factor=0.5,
+                 verify=True, proxies=None, auth=None):
+        """RetrySession constructor.
+
+        Args:
+            base_url (str): base URL for the session's requests.
+            access_token (str): IBM Q access token.
+            retries (int): number of retries for the requests.
+            backoff_factor (float): backoff factor between retry attempts.
+            verify (bool): enable SSL verification.
+            proxies (dict): proxy URLs mapped by protocol.
+            auth (AuthBase): authentication handler.
+        """
+        super().__init__()
+
+        self.base_url = base_url
+
+        self._initialize_retry(retries, backoff_factor)
+        self._initialize_session_parameters(
+            access_token, verify, proxies or {}, auth)
+
+    def _initialize_retry(self, retries, backoff_factor):
+        """Set the Session retry policy.
+
+        Args:
+            retries (int): number of retries for the requests.
+            backoff_factor (float): backoff factor between retry attempts.
+        """
+        retry = Retry(
+            total=retries,
+            backoff_factor=backoff_factor,
+            status_forcelist=STATUS_FORCELIST,
+        )
+
+        retry_adapter = HTTPAdapter(max_retries=retry)
+        self.mount('http://', retry_adapter)
+        self.mount('https://', retry_adapter)
+
+    def _initialize_session_parameters(self, access_token,
+                                       verify, proxies, auth):
+        """Set the Session parameters and attributes.
+
+        Args:
+            access_token (str): IBM Q access token.
+            verify (bool): enable SSL verification.
+            proxies (dict): proxy URLs mapped by protocol.
+            auth (AuthBase): authentication handler.
+        """
+        self.headers.update({'X-Qx-Client-Application': CLIENT_APPLICATION})
+        if access_token:
+            self.params.update({'access_token': access_token})
+
+        self.auth = auth
+        self.proxies = proxies or {}
+        self.verify = verify
+
+    def request(self, method, url, **kwargs):
+        """Constructs a Request, prepending the base url."""
+        full_url = self.base_url + url
+
+        return super().request(method, full_url, **kwargs)
