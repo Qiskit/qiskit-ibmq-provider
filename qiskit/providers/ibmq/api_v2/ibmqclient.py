793913f39e0b87e2c36113e44d1c9670c752f456
Max Reuter <max.reuter@ibm.com>
2019-05-23 15:44:57 -0400

    Add proxy args

--- qiskit/providers/ibmq/api_v2/ibmqclient.py
+++ qiskit/providers/ibmq/api_v2/ibmqclient.py
@@ -25,7 +25,7 @@ from .exceptions import RequestsApiError, AuthenticationLicenseError
 class IBMQClient:
     """Client for programmatic access to the IBM Q API."""
 
-    def __init__(self, api_token, auth_url):
+    def __init__(self, api_token, auth_url, proxies=None):
         """IBMQClient constructor.
 
         Args:
@@ -34,8 +34,9 @@ class IBMQClient:
         """
         self.api_token = api_token
         self.auth_url = auth_url
+        self.proxies = proxies
 
-        self.client_auth = Auth(RetrySession(auth_url))
+        self.client_auth = Auth(RetrySession(auth_url, proxies=self.proxies))
         self.client_api, self.client_ws = self._init_service_clients()
 
     def _init_service_clients(self):
@@ -53,7 +54,7 @@ class IBMQClient:
         service_urls = self._user_urls()
 
         # Create the api server client, using the access token.
-        client_api = Api(RetrySession(service_urls['http'], access_token))
+        client_api = Api(RetrySession(service_urls['http'], access_token, proxies=self.proxies))
 
         # Create the websocket server client, using the access token.
         client_ws = WebsocketClient(service_urls['ws'], access_token)
