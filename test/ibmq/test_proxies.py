c92b263a18b49addde029b554e6c958eea336b6d
Max Reuter <max.reuter@ibm.com>
2019-05-30 09:43:16 -0400

    WIP: proxy tests

--- test/ibmq/test_proxies.py
+++ test/ibmq/test_proxies.py
@@ -26,19 +26,39 @@ from qiskit.test import QiskitTestCase, requires_qe_access
 
 from ..decorators import requires_new_api_auth
 
+import asyncio
+import pproxy as pp
+
 
 class TestProxies(QiskitTestCase):
-    """Tests for proxies."""
+    """Tests for proxy capabilities."""
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        # Launch the mock server.
+        server = pp.Server('ss://127.0.0.1:8080')
+        remote = pp.Connection('ss://127.0.0.1:8080')
+        args = dict(rserver=[remote],
+                    verbose=print)
+        cls.server = asyncio.get_event_loop().run_until_complete(server.start_server(args))
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+
+        # Close the mock server.
+        loop = asyncio.get_event_loop()
+        loop.stop()
 
     @requires_qe_access
     @requires_new_api_auth
     def test_proxies(self, qe_token, qe_url):
         """Test IBMQClient proxy support."""
-        return  # skip for now
-
         proxies = {
-            'http': '127.0.0.1:8080',
             'https': '127.0.0.1:8080'
         }
 
         IBMQClient(qe_token, qe_url, proxies)
+        return
