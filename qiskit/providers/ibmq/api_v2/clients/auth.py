90ec28a155a1dbd5ed0bbf681916111b1f492580
Diego M. Rodriguez <diego.plan9@gmail.com>
2019-06-25 16:51:43 +0200

    Fix AuthClient docstrings

--- qiskit/providers/ibmq/api_v2/clients/auth.py
+++ qiskit/providers/ibmq/api_v2/clients/auth.py
@@ -12,7 +12,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Client for accessing authentication features of IBM Q ."""
+"""Client for accessing authentication features of IBM Q."""
 
 from ..exceptions import AuthenticationLicenseError, RequestsApiError
 from ..rest import Api, Auth
@@ -22,7 +22,7 @@ from .base import BaseClient
 
 
 class AuthClient(BaseClient):
-    """Client for programmatic access to the IBM Q API."""
+    """Client for accessing authentication features of IBM Q."""
 
     def __init__(self, api_token, auth_url, **request_kwargs):
         """IBMQClient constructor.
