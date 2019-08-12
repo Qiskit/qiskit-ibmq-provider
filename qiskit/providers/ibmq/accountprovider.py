e676764c5f820d504b0896d46189192a85078c23
Jessie Yu <jessieyu@us.ibm.com>
2019-08-12 17:23:01 -0400

    move provider backend to separate file

--- qiskit/providers/ibmq/accountprovider.py
+++ qiskit/providers/ibmq/accountprovider.py
@@ -15,10 +15,7 @@
 """Provider for a single IBM Quantum Experience account."""
 
 import logging
-import re
-import keyword
 from collections import OrderedDict
-from types import SimpleNamespace
 
 from qiskit.providers import BaseProvider
 from qiskit.providers.models import (QasmBackendConfiguration,
@@ -27,9 +24,9 @@ from qiskit.providers.providerutils import filter_backends
 from qiskit.validation.exceptions import ModelValidationError
 
 from .api_v2.clients import AccountClient
-from .api_v2.exceptions import RequestsApiError
 from .circuits import CircuitsManager
 from .ibmqbackend import IBMQBackend, IBMQSimulator
+from .providerbackends import ProviderBackends
 
 logger = logging.getLogger(__name__)
 
@@ -166,50 +163,3 @@ class AccountProvider(BaseProvider):
 
         return "<{} for IBMQ({})>".format(
             self.__class__.__name__, credentials_info)
-
-
-class ProviderBackends(SimpleNamespace):
-    """Backend namespace for the provider."""
-
-    def __init__(self, provider):
-        """Creates a new ProviderBackends instance.
-
-        Args:
-            provider (AccountProvider): IBM Q Experience account provider
-        """
-        self._provider = provider
-        self._initialized = False
-        super().__init__()
-
-    def _discover_backends(self):
-        """Discovers the remote backends if not already known."""
-        if not self._initialized:
-            try:
-                # Python identifiers can only contain alphanumeric characters
-                # and underscores and cannot start with a digit.
-                pattern = re.compile(r"\W|^(?=\d)", re.ASCII)
-                for backend in self._provider.backends(timeout=3):
-                    backend_name = backend.name()
-
-                    # Make it a valid identifier
-                    if not backend_name.isidentifier():
-                        backend_name = re.sub(pattern, '_', backend_name)
-
-                    # Append _ if is keyword or duplicate
-                    while keyword.iskeyword(backend_name) or backend_name in self.__dict__:
-                        backend_name += '_'
-
-                    setattr(self, backend_name, backend)
-                self._initialized = True
-            except RequestsApiError:
-                # Ignore any networking errors since this is a convenience
-                # feature meant for interactive sessions.
-                pass
-
-    def __dir__(self):
-        self._discover_backends()
-        return super().__dir__()
-
-    def __getattr__(self, item):
-        self._discover_backends()
-        return super().__getattribute__(item)
