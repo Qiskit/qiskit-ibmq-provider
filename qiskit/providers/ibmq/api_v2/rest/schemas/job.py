e461b26f2a7f6b24cc2acdd1bcb5c312959c7376
Daniyar Yeralin <daniyar.yeralin@gmail.com>
2019-08-22 15:22:36 -0400

    Introduce job endpoints schemas

---
+++ qiskit/providers/ibmq/api_v2/rest/schemas/job.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2017, 2019.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Schemas for job."""
+from marshmallow.validate import OneOf
+from qiskit.providers.ibmq.apiconstants import ApiJobStatus
+from qiskit.validation import BaseSchema
+from qiskit.validation.fields import String, Url, Nested, Number, Boolean
+
+
+# Helper schemas.
+
+class InfoQueueResponseSchema(BaseSchema):
+    """Nested schema for StatusResponseSchema"""
+
+    # Required properties
+    status = String(required=True)
+    position = Number(required=True, missing=0)
+
+
+class JobResponseSchema(BaseSchema):
+    """Nested schema for CallbackUploadResponseSchema"""
+    # pylint: disable=invalid-name
+
+    # Required properties
+    id = String(required=True)
+    kind = String(required=True)
+
+
+# Endpoint schemas.
+
+class PropertiesResponseSchema(BaseSchema):
+    """Schema for PropertiesResponse"""
+    pass
+
+
+class StatusResponseSchema(BaseSchema):
+    """Schema for StatusResponse"""
+
+    # Optional properties
+    infoQueue = Nested(InfoQueueResponseSchema, required=False)
+
+    # Required properties
+    status = String(required=True, validate=OneOf([status.name for status in ApiJobStatus]))
+
+
+class CancelResponseSchema(BaseSchema):
+    """Schema for CancelResponse"""
+
+    # Optional properties
+    error = String(required=False)
+
+    # Required properties
+    cancelled = Boolean(required=True, description="True if job can be cancelled, else False. "
+                                                   "Note this operation might not be possible "
+                                                   "depending on the environment.")
+
+
+class UploadUrlResponseSchema(BaseSchema):
+    """Schema for UploadUrlResponse"""
+
+    # Required properties
+    upload_url = Url(required=True, description="upload object storage URL.")
+
+
+class DownloadUrlResponseSchema(BaseSchema):
+    """Schema for DownloadUrlResponse"""
+
+    # Required properties
+    download_url = Url(required=True, description="download object storage URL.")
+
+
+class ResultUrlResponseSchema(BaseSchema):
+    """Schema for ResultUrlResponse"""
+
+    # Required properties
+    download_url = Url(required=True, description="object storage URL.")
+
+
+class CallbackUploadResponseSchema(BaseSchema):
+    """Schema for CallbackUploadResponse"""
+
+    # Required properties
+    job = Nested(JobResponseSchema, required=True)
+
+
+class CallbackDownloadResponseSchema(BaseSchema):
+    """Schema for CallbackDownloadResponse"""
+    pass
