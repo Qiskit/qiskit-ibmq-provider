162d895ff97d3c2224426042d524d9af5d790b5e
Diego M. Rodriguez <diego.plan9@gmail.com>
2019-09-18 13:05:06 +0200

    Remove deprecated v1 methods from ibmqfactory

--- qiskit/providers/ibmq/ibmqfactory.py
+++ qiskit/providers/ibmq/ibmqfactory.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 
 # This code is part of Qiskit.
@@ -19,8 +20,6 @@ import warnings
 from typing import Dict, List, Union, Optional, Any
 from collections import OrderedDict
 
-from qiskit.providers.exceptions import QiskitBackendNotFoundError
-
 from .accountprovider import AccountProvider
 from .api_v2.clients import AuthClient, VersionClient
 from .credentials import Credentials, discover_credentials
@@ -29,7 +28,7 @@ from .credentials.configrc import (read_credentials_from_qiskitrc,
                                    store_credentials)
 from .credentials.updater import update_credentials
 from .exceptions import IBMQAccountError, IBMQApiUrlError, IBMQProviderError
-from .utils.deprecation import deprecated, UPDATE_ACCOUNT_TEXT
+from .utils.deprecation import UPDATE_ACCOUNT_TEXT
 
 
 logger = logging.getLogger(__name__)
@@ -54,10 +53,9 @@ class IBMQFactory:
     ) -> Optional[AccountProvider]:
         """Authenticate against IBM Q Experience for use during this session.
 
-        Note: with version 0.3 of this qiskit-ibmq-provider package, use of
+        Note: with version 0.4 of this qiskit-ibmq-provider package, use of
             the legacy Quantum Experience and Qconsole (also known
-            as the IBM Q Experience v1) credentials is deprecated. The new
-            default is to use the IBM Q Experience v2 credentials.
+            as the IBM Q Experience v1) credentials is fully deprecated.
 
         Args:
             token (str): IBM Q Experience API token.
@@ -141,7 +139,7 @@ class IBMQFactory:
 
         # Check the URL is a valid authentication URL.
         if not version_info['new_api'] or 'api-auth' not in version_info:
-            raise IBMQAccountError('Invalid IBM Q Experience v2 credentials '
+            raise IBMQAccountError('Invalid IBM Q Experience credentials '
                                    'found. ' + UPDATE_ACCOUNT_TEXT)
 
         # Initialize the providers.
@@ -375,203 +373,3 @@ class IBMQFactory:
                 # Catch-all for errors instantiating the provider.
                 logger.warning('Unable to instantiate provider for %s: %s',
                                hub_info, ex)
-
-    # Deprecated account management functions for backward compatibility.
-
-    @deprecated
-    def active_accounts(self):  # type: ignore
-        """List all IBM Q Experience v1 accounts currently in the session.
-
-        Note: this method is being deprecated, and is only available when using
-            v1 accounts.
-
-        Returns:
-            list[dict]: a list with information about the accounts currently
-                in the session.
-
-        Raises:
-            IBMQAccountError: if the method is used with an IBM Q Experience
-                v2 account.
-        """
-        return self._v1_provider.active_accounts()
-
-    @deprecated
-    def disable_accounts(self, **kwargs):  # type: ignore
-        """Disable IBM Q Experience v1 accounts in the current session.
-
-        Note: this method is being deprecated, and only available when using
-            v1 accounts.
-
-        The filter kwargs can be `token`, `url`, `hub`, `group`, `project`.
-        If no filter is passed, all accounts in the current session will be disabled.
-
-        Raises:
-            IBMQAccountError: if the method is used with an IBM Q Experience v2
-                account, or if no account matched the filter.
-        """
-        self._v1_provider.disable_accounts(**kwargs)
-
-    @deprecated
-    def load_accounts(self, **kwargs):  # type: ignore
-        """Load IBM Q Experience v1 accounts found in the system into current session.
-
-        Will also load v2 accounts for backward compatibility, but can lead to
-        issues if mixing v1 and v2 credentials in other method calls.
-
-        Note: this method is being deprecated, and only available when using
-            v1 accounts.
-
-        Automatically load the accounts found in the system. This method
-        looks for credentials in the following locations, in order, and
-        returns as soon as credentials are found:
-
-        1. in the `Qconfig.py` file in the current working directory.
-        2. in the environment variables.
-        3. in the `qiskitrc` configuration file
-
-        Raises:
-            IBMQAccountError: If mixing v1 and v2 account credentials
-        """
-        version_counter = 0
-        for credentials in discover_credentials().values():
-            # Explicitly check via an API call, to prevent credentials that
-            # contain API 2 URL (but not auth) slipping through.
-            version_info = self._check_api_version(credentials)
-            version_counter += int(version_info['new_api'])
-
-        # Check if mixing v1 and v2 credentials
-        if version_counter != 0 and version_counter < len(discover_credentials().values()):
-            raise IBMQAccountError('Can not mix API v1 and v2'
-                                   'credentials.')
-
-        # If calling using API v2 credentials
-        if version_counter:
-            warnings.warn(
-                'Calling IBMQ.load_accounts() with v2 credentials. '
-                'This is provided for backwards compatibility '
-                'and may lead to unexpected behaviour when mixing '
-                'v1 and v2 account credentials.', DeprecationWarning)
-            self.load_account()
-        else:
-            # Old API v1 call
-            self._v1_provider.load_accounts(**kwargs)
-
-    @deprecated
-    def delete_accounts(self, **kwargs):  # type: ignore
-        """Delete saved IBM Q Experience v1 accounts from disk, subject to optional filtering.
-
-        Note: this method is being deprecated, and only available when using
-            v1 accounts.
-
-        The filter kwargs can be `token`, `url`, `hub`, `group`, `project`.
-        If no filter is passed, all accounts will be deleted from disk.
-
-        Raises:
-            IBMQAccountError: if the method is used with an IBM Q Experience v2
-                account, or if no account matched the filter.
-        """
-        self._v1_provider.delete_accounts(**kwargs)
-
-    @deprecated
-    def stored_accounts(self):  # type: ignore
-        """List all IBM Q Experience v1 accounts stored to disk.
-
-        Note: this method is being deprecated, and only available when using
-            v1 accounts.
-
-        Returns:
-            list[dict]: a list with information about the accounts stored
-                on disk.
-
-        Raises:
-            IBMQAccountError: if the method is used with an IBM Q Experience v2 account.
-        """
-        return self._v1_provider.stored_accounts()
-
-    # Deprecated backend-related functionality.
-
-    def backends(self, name=None, filters=None, **kwargs):  # type: ignore
-        """Return all backends accessible via IBMQ provider, subject to optional filtering.
-
-        Note: this method is being deprecated. Please use an IBM Q Experience v2
-            account, and::
-
-            provider = IBMQ.get_provider(...)
-            provider.backends()
-
-            instead.
-
-        Args:
-            name (str): backend name to filter by
-            filters (callable): more complex filters, such as lambda functions
-                e.g. IBMQ.backends(filters=lambda b: b.configuration['n_qubits'] > 5)
-            kwargs: simple filters specifying a true/false criteria in the
-                backend configuration or backend status or provider credentials
-                e.g. IBMQ.backends(n_qubits=5, operational=True, hub='internal')
-
-        Returns:
-            list[IBMQBackend]: list of backends available that match the filter
-        """
-        warnings.warn('IBMQ.backends() is being deprecated. '
-                      'Please use IBMQ.get_provider() to retrieve a provider '
-                      'and AccountProvider.backends() to find its backends.',
-                      DeprecationWarning)
-
-        if self._credentials:
-            hgp_filter = {}
-
-            # First filter providers by h/g/p
-            for key in ['hub', 'group', 'project']:
-                if key in kwargs:
-                    hgp_filter[key] = kwargs.pop(key)
-            providers = self.providers(**hgp_filter)
-
-            # Aggregate the list of filtered backends.
-            backends = []
-            for provider in providers:
-                backends = backends + provider.backends(
-                    name=name, filters=filters, **kwargs)
-
-            return backends
-        else:
-            return self._v1_provider.backends(name, filters, **kwargs)
-
-    def get_backend(self, name=None, **kwargs):  # type: ignore
-        """Return a single backend matching the specified filtering.
-
-        Note: this method is being deprecated. Please use an IBM Q Experience v2
-            account, and::
-
-            provider = IBMQ.get_provider(...)
-            provider.get_backend('name')
-
-            instead.
-
-        Args:
-            name (str): name of the backend.
-            **kwargs (dict): dict used for filtering.
-
-        Returns:
-            BaseBackend: a backend matching the filtering.
-
-        Raises:
-            QiskitBackendNotFoundError: if no backend could be found or
-                more than one backend matches the filtering criteria.
-        """
-        warnings.warn('IBMQ.get_backend() is being deprecated. '
-                      'Please use IBMQ.get_provider() to retrieve a provider '
-                      'and AccountProvider.get_backend("name") to retrieve a '
-                      'backend.',
-                      DeprecationWarning)
-
-        # Don't issue duplicate warnings
-        with warnings.catch_warnings():
-            warnings.filterwarnings('ignore', category=DeprecationWarning)
-            backends = self.backends(name, **kwargs)
-
-        if len(backends) > 1:
-            raise QiskitBackendNotFoundError('More than one backend matches the criteria')
-        if not backends:
-            raise QiskitBackendNotFoundError('No backend matches the criteria')
-
-        return backends[0]
