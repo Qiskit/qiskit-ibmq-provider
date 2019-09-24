4c86b6734dbe3d7c90790d51d2ca5a038b4d82ae
Diego M. Rodríguez <diego.plan9@gmail.com>
2019-09-24 17:58:57 +0200

    Wrap up v1 deprecation (#348)
    * Remove v1 references
    
    * Update v1 and v2 strings from test_ibmq_factory
    
    * Move UPDATE_ACCOUNT_TEXT to ibmqfactory.py
    
    * Remove qiskit/providers/ibmq/utils/deprecation.py
    
    * Remove api-specific decorators
    
    * Remove old api tests from travis
    
    * Tweak README.md
    
    * Remove more v2 references (thanks Luis!)

--- qiskit/providers/ibmq/api/rest/root.py
+++ qiskit/providers/ibmq/api/rest/root.py
@@ -12,7 +12,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Root REST adapter for the IBM Q Experience v2 API."""
+"""Root REST adapter for the IBM Q Experience API."""
 
 import json
 
