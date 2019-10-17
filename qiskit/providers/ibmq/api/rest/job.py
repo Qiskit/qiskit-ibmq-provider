9e4ba048da4b412545a8f3ffda6c394fdd5674ac
jessieyu <jessieyu@us.ibm.com>
2019-10-17 09:06:34 -0400

    fix import

--- qiskit/providers/ibmq/api/rest/job.py
+++ qiskit/providers/ibmq/api/rest/job.py
@@ -20,7 +20,7 @@ from typing import Dict, Any
 from marshmallow.exceptions import ValidationError
 
 from .base import RestAdapterBase
-from qiskit.providers.ibmq.api.rest.validation import StatusResponseSchema
+from .validation import StatusResponseSchema
 from ..session import RetrySession
 from ..exceptions import ApiIBMQProtocolError
 
