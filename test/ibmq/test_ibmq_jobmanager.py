2216e758d65e48a8cab49826dd769c14de5619e8
Jessie Yu <jessieyu@us.ibm.com>
2019-10-30 08:29:41 -0400

    Managed results (#407)
    * add managed job set
    
    * bug fixes
    
    * add managed results
    
    * fix rebase issues
    
    * fix test
    
    * fix type hint
    
    * partial result support
    
    * change test to use simulator
    
    * update CHANGELOG

--- test/ibmq/test_ibmq_jobmanager.py
+++ test/ibmq/test_ibmq_jobmanager.py
@@ -19,15 +19,16 @@ import time
 
 from qiskit import QuantumCircuit
 from qiskit.providers.ibmq.managed.ibmqjobmanager import IBMQJobManager
+from qiskit.providers.ibmq.managed.exceptions import (IBMQJobManagerJobNotFound,
+                                                      IBMQManagedResultDataNotAvailable)
 from qiskit.providers.jobstatus import JobStatus
-from qiskit.providers.ibmq import least_busy
 from qiskit.providers import JobError
 from qiskit.providers.ibmq.ibmqbackend import IBMQBackend
 from qiskit.providers.ibmq.exceptions import IBMQBackendError
-from qiskit.compiler import transpile
+from qiskit.compiler import transpile, assemble
 
 from ..ibmqtestcase import IBMQTestCase
-from ..decorators import requires_provider, run_on_staging
+from ..decorators import requires_provider
 from ..fake_account_client import BaseFakeAccountClient
 
 
@@ -35,11 +36,7 @@ class TestIBMQJobManager(IBMQTestCase):
     """Tests for IBMQJobManager."""
 
     def setUp(self):
-        self._qc = QuantumCircuit(2, 2)
-        self._qc.h(0)
-        self._qc.cx(0, 1)
-        self._qc.measure([0, 1], [0, 1])
-
+        self._qc = _bell_circuit()
         self._jm = IBMQJobManager()
 
     @requires_provider
@@ -53,12 +50,12 @@ class TestIBMQJobManager(IBMQTestCase):
         for _ in range(max_circs+2):
             circs.append(self._qc)
         job_set = self._jm.run(circs, backend=backend)
-        results = job_set.results()
+        job_set.results()
         statuses = job_set.statuses()
 
-        self.assertEqual(len(results), 2)
         self.assertEqual(len(statuses), 2)
         self.assertTrue(all(s is JobStatus.DONE for s in statuses))
+        self.assertTrue(len(job_set.jobs()), 2)
 
     @requires_provider
     def test_no_split_circuits(self, provider):
@@ -71,11 +68,7 @@ class TestIBMQJobManager(IBMQTestCase):
         for _ in range(int(max_circs/2)):
             circs.append(self._qc)
         job_set = self._jm.run(circs, backend=backend)
-        results = job_set.results()
-        statuses = job_set.statuses()
-
-        self.assertEqual(len(results), 1)
-        self.assertEqual(len(statuses), 1)
+        self.assertTrue(len(job_set.jobs()), 1)
 
     @requires_provider
     def test_custom_split_circuits(self, provider):
@@ -87,29 +80,7 @@ class TestIBMQJobManager(IBMQTestCase):
         for _ in range(2):
             circs.append(self._qc)
         job_set = self._jm.run(circs, backend=backend, max_experiments_per_job=1)
-        results = job_set.results()
-        statuses = job_set.statuses()
-
-        self.assertEqual(len(results), 2)
-        self.assertEqual(len(statuses), 2)
-
-    @requires_provider
-    def test_result(self, provider):
-        """Test getting results for multiple jobs."""
-        backend = provider.get_backend('ibmq_qasm_simulator')
-        backend._api = BaseFakeAccountClient()
-
-        circs = []
-        for _ in range(2):
-            circs.append(self._qc)
-        job_set = self._jm.run(circs, backend=backend, max_experiments_per_job=1)
-        results = job_set.results()
-        jobs = job_set.jobs()
-
-        self.assertEqual(len(results), 2)
-        for i, result in enumerate(results):
-            self.assertIsNotNone(result)
-            self.assertDictEqual(result.get_counts(0), jobs[i].result().get_counts(0))
+        self.assertTrue(len(job_set.jobs()), 2)
 
     @requires_provider
     def test_job_report(self, provider):
@@ -125,37 +96,6 @@ class TestIBMQJobManager(IBMQTestCase):
         report = self._jm.report()
         for job in jobs:
             self.assertIn(job.job_id(), report)
-
-    @requires_provider
-    def test_skipped_result(self, provider):
-        """Test one of jobs has no result."""
-        backend = provider.get_backend('ibmq_qasm_simulator')
-        max_circs = backend.configuration().max_experiments
-
-        circs = []
-        for _ in range(max_circs+2):
-            circs.append(self._qc)
-        job_set = self._jm.run(circs, backend=backend)
-        jobs = job_set.jobs()
-        cjob = jobs[0]
-        cancelled = False
-        for _ in range(2):
-            # Try twice in case job is not in a cancellable state
-            try:
-                cancelled = cjob.cancel()
-                if cancelled:
-                    break
-            except JobError:
-                pass
-
-        results = job_set.results()
-        statuses = job_set.statuses()
-        if cancelled:
-            self.assertTrue(statuses[0] is JobStatus.CANCELLED)
-            self.assertIsNone(
-                results[0], "Job {} cancelled but result is not None.".format(cjob.job_id()))
-        else:
-            self.log.warning("Unable to cancel job %s", cjob.job_id())
 
     @requires_provider
     def test_skipped_status(self, provider):
@@ -186,22 +126,27 @@ class TestIBMQJobManager(IBMQTestCase):
             rjob = provider.backends.retrieve_job(jobs[i].job_id())
             self.assertDictEqual(qobj.__dict__, rjob.qobj().__dict__)
 
-    @run_on_staging
+    @requires_provider
     def test_error_message(self, provider):
         """Test error message report."""
-        backend = least_busy(provider.backends(simulator=False))
+        backend = provider.get_backend('ibmq_qasm_simulator')
 
-        bad_qc = copy.deepcopy(self._qc)
-        circs = [transpile(self._qc, backend=backend), bad_qc]
-        job_set = self._jm.run(circs, backend=backend, max_experiments_per_job=1)
+        # Create a bad job.
+        qc_new = transpile(self._qc, backend)
+        qobj = assemble([qc_new, qc_new], backend=backend)
+        qobj.experiments[1].instructions[1].name = 'bad_instruction'
+        job = backend.run(qobj)
 
-        jobs = job_set.jobs()
-        results = job_set.results(timeout=300)
-        self.assertIsNone(results[1])
+        circs = []
+        for _ in range(4):
+            circs.append(self._qc)
+        job_set = self._jm.run(circs, backend=backend, max_experiments_per_job=2)
+        job_set.results()
+        job_set.managed_jobs()[1].job = job
 
         error_report = job_set.error_messages()
         self.assertIsNotNone(error_report)
-        self.assertIn(jobs[1].job_id(), error_report)
+        self.assertIn(job.job_id(), error_report)
 
     @requires_provider
     def test_async_submit_exception(self, provider):
@@ -251,3 +196,97 @@ class TestIBMQJobManager(IBMQTestCase):
                                name=name, max_experiments_per_job=1)
         rjob_set = self._jm.job_sets(name=name)[0]
         self.assertEqual(job_set, rjob_set)
+
+
+class TestResultManager(IBMQTestCase):
+    """Tests for ResultManager."""
+
+    def setUp(self):
+        self._qc = _bell_circuit()
+        self._jm = IBMQJobManager()
+
+    @requires_provider
+    def test_index_by_number(self, provider):
+        """Test indexing results by number."""
+        backend = provider.get_backend('ibmq_qasm_simulator')
+        max_per_job = 5
+        circs = []
+        for _ in range(max_per_job*2):
+            circs.append(self._qc)
+        job_set = self._jm.run(circs, backend=backend, max_experiments_per_job=max_per_job)
+        result_manager = job_set.results()
+        jobs = job_set.jobs()
+
+        for i in [0, max_per_job-1, max_per_job+1]:
+            with self.subTest(i=i):
+                job_index = int(i / max_per_job)
+                exp_index = i % max_per_job
+                self.assertEqual(result_manager.get_counts(i),
+                                 jobs[job_index].result().get_counts(exp_index))
+
+    @requires_provider
+    def test_index_by_name(self, provider):
+        """Test indexing results by name."""
+        backend = provider.get_backend('ibmq_qasm_simulator')
+        max_per_job = 5
+        circs = []
+        for i in range(max_per_job*2+1):
+            new_qc = copy.deepcopy(self._qc)
+            new_qc.name = "test_qc_{}".format(i)
+            circs.append(new_qc)
+        job_set = self._jm.run(circs, backend=backend, max_experiments_per_job=max_per_job)
+        result_manager = job_set.results()
+        jobs = job_set.jobs()
+
+        for i in [1, max_per_job, len(circs)-1]:
+            with self.subTest(i=i):
+                job_index = int(i / max_per_job)
+                exp_index = i % max_per_job
+                self.assertEqual(result_manager.get_counts(circs[i].name),
+                                 jobs[job_index].result().get_counts(exp_index))
+
+    @requires_provider
+    def test_index_out_of_range(self, provider):
+        """Test result index out of range."""
+        backend = provider.get_backend('ibmq_qasm_simulator')
+        job_set = self._jm.run([self._qc], backend=backend)
+        result_manager = job_set.results()
+        with self.assertRaises(IBMQJobManagerJobNotFound):
+            result_manager.get_counts(1)
+
+    @requires_provider
+    def test_skipped_result(self, provider):
+        """Test one of jobs has no result."""
+        backend = provider.get_backend('ibmq_qasm_simulator')
+        max_circs = backend.configuration().max_experiments
+
+        circs = []
+        for _ in range(max_circs+2):
+            circs.append(self._qc)
+        job_set = self._jm.run(circs, backend=backend)
+        jobs = job_set.jobs()
+        cjob = jobs[1]
+        cancelled = False
+        for _ in range(2):
+            # Try twice in case job is not in a cancellable state
+            try:
+                cancelled = cjob.cancel()
+                if cancelled:
+                    break
+            except JobError:
+                pass
+
+        result_manager = job_set.results()
+        if cancelled:
+            with self.assertRaises(IBMQManagedResultDataNotAvailable):
+                result_manager.get_counts(max_circs)
+        else:
+            self.log.warning("Unable to cancel job %s", cjob.job_id())
+
+
+def _bell_circuit():
+    qc = QuantumCircuit(2, 2)
+    qc.h(0)
+    qc.cx(0, 1)
+    qc.measure([0, 1], [0, 1])
+    return qc
