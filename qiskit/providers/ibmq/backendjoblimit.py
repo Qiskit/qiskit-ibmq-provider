a781925a64eaec1babcf336ed49ff8111e2efd58
Luis Garcia <luisg5@uci.edu>
2020-01-14 09:24:21 -0500

    remove backend dir

--- qiskit/providers/ibmq/backendjoblimit.py
+++ qiskit/providers/ibmq/backendjoblimit.py
@@ -18,7 +18,7 @@ from typing import Any
 
 from qiskit.validation import BaseModel, bind_schema
 
-from ..api.rest.validation import BackendJobLimitResponseSchema
+from qiskit.providers.ibmq.api.rest.validation import BackendJobLimitResponseSchema
 
 
 @bind_schema(BackendJobLimitResponseSchema)
