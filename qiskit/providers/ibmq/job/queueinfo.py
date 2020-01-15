b8f2a349c9feb2efc0d22a9f25eb557e4d250098
Luis Garcia <luisg5@uci.edu>
2020-01-15 10:57:03 -0500

    add default values to formatgit status

--- qiskit/providers/ibmq/job/queueinfo.py
+++ qiskit/providers/ibmq/job/queueinfo.py
@@ -14,7 +14,7 @@
 
 """Queue information related to a job."""
 
-from typing import Any, Optional
+from typing import Dict, Any, Optional
 from datetime import datetime
 
 import arrow
@@ -23,7 +23,6 @@ from qiskit.validation import BaseModel, bind_schema
 
 from ..api.rest.validation import InfoQueueResponseSchema
 from ..apiconstants import ApiJobStatus
-
 from .utils import api_status_to_job_status
 
 
@@ -53,7 +52,7 @@ class QueueInfo(BaseModel):
             hub_priority: Dynamic priority for the hub.
             group_priority: Dynamic priority for the group.
             project_priority: Dynamic priority for the project.
-            job_id: The id of the job.
+            job_id: The ID of the job.
             kwargs: additional attributes that will be added as instance members.
         """
         self.position = position
@@ -73,19 +72,40 @@ class QueueInfo(BaseModel):
         Returns:
              The job queue information report.
         """
-        job_status = api_status_to_job_status(ApiJobStatus(self._status))
-        estimated_start_time = arrow.get(self.estimated_start_time).humanize()
-        estimated_completion_time = arrow.get(self.estimated_complete_time).humanize()
+        _default_attr_value = 'unknown'
+        attributes_map = self._get_attributes_map(_default_attr_value)
+
+        status = attributes_map['_status']
+        if status != _default_attr_value:
+            attributes_map['_status'] = api_status_to_job_status(ApiJobStatus(status)).value
+
+        est_start_time = attributes_map['estimated_start_time']
+        if est_start_time != _default_attr_value:
+            attributes_map['estimated_start_time'] = arrow.get(est_start_time).humanize()
+
+        est_complete_time = attributes_map['estimated_complete_time']
+        if est_complete_time != _default_attr_value:
+            attributes_map['estimated_complete_time'] = arrow.get(est_complete_time).humanize()
 
         queue_info = [
-            "Job {} queue information:".format(self.job_id),
-            "    queue position: {}".format(self.position),
-            "    status: {}".format(job_status.value),
-            "    estimated start time: {}".format(estimated_start_time),
-            "    estimated completion time: {}".format(estimated_completion_time),
-            "    hub priority: {}".format(self.hub_priority),
-            "    group priority: {}".format(self.group_priority),
-            "    project priority: {}".format(self.project_priority)
+            "Job {} queue information:".format(attributes_map['job_id']),
+            "    queue position: {}".format(attributes_map['position']),
+            "    status: {}".format(attributes_map['_status']),
+            "    estimated start time: {}".format(attributes_map['estimated_start_time']),
+            "    estimated completion time: {}".format(attributes_map['estimated_complete_time']),
+            "    hub priority: {}".format(attributes_map['hub_priority']),
+            "    group priority: {}".format(attributes_map['group_priority']),
+            "    project priority: {}".format(attributes_map['project_priority'])
         ]
 
         return '\n'.join(queue_info)
+
+    def _get_attributes_map(self, _default_value: str) -> Dict[str, Any]:
+        """Utility function to get the attributes map, while setting default values."""
+        _attributes_map = {}
+        for key, value in self.__dict__.items():
+            if value is None:
+                _attributes_map[key] = _default_value
+            else:
+                _attributes_map[key] = value
+        return _attributes_map
