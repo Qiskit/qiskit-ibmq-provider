7e1bf13cdcbb7072c82f61a5f92312ece6f9c857
Paul Nation <nonhermitian@gmail.com>
2020-02-06 15:23:58 -0500

    Start of visualization migration / new stuff (#535)
    * start adding visualizations to IBM Quantum API
    
    * updates
    
    * ignore issue that blocks docs formatting
    
    * add iplot_gate_map
    
    * add stubs to ignore
    
    * lint
    
    * add jobs widget
    
    * more widgets
    
    * updates
    
    * margins and kernel repr
    
    * style and lint
    
    * add jupyter section to docs
    
    * move to jupyter execute
    
    Co-authored-by: Jessie Yu <jessieyu@us.ibm.com>

---
+++ qiskit/providers/ibmq/visualization/colormaps.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2017, 2019.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""An interactive backend error map.
+"""
+import numpy as np
+import seaborn as sns
+
+HELIX_LIGHT_CMAP = sns.cubehelix_palette(start=2.5,
+                                         rot=0.6,
+                                         hue=2,
+                                         gamma=1,
+                                         light=0.9,
+                                         dark=0.15,
+                                         reverse=True,
+                                         as_cmap=True)
+
+HELIX_DARK_CMAP = sns.cubehelix_palette(start=2.5,
+                                        rot=0.6,
+                                        hue=2,
+                                        gamma=0.95,
+                                        light=0.95,
+                                        dark=0.25,
+                                        reverse=True,
+                                        as_cmap=True)
+
+
+def _sns_to_plotly(cmap, pl_entries=255):
+    hgt = 1.0/(pl_entries-1)
+    pl_colorscale = []
+
+    for k in range(pl_entries):
+        clr = list(map(np.uint8, np.array(cmap(k*hgt)[:3])*255))
+        pl_colorscale.append([k*hgt, 'rgb'+str((clr[0], clr[1], clr[2]))])
+
+    return pl_colorscale
+
+
+HELIX_LIGHT = _sns_to_plotly(HELIX_LIGHT_CMAP)
+HELIX_DARK = _sns_to_plotly(HELIX_DARK_CMAP)
