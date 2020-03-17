62b42586f25e0577d512dc155045da6c7ee32b03
Luis Garcia <luisg5@uci.edu>
2020-03-17 17:02:01 -0400

    Improve logging (#579)
    * Setup logger for package.
    
    * Add job submission logging messages.
    
    * revise log message
    
    * review comments
    
    * update docstring.
    
    * seperate console and file logging.
    
    * remove logging from rest endpoints.
    
    * add logging to session.request
    
    * refactor setup_logger
    
    * update docstring
    
    * censor the apiToken
    
    * use appropriate formatting for logging
    
    * review comments
    
    * write tests for logger.
    
    * fix lint
    
    * update docstrings.
    
    * add response data
    
    * update changelog
    
    * style fixes
    
    * update changelog
    
    * review comments for tests.
    
    * move log level setting placement.
    
    * update docs
    
    * add chained exception logging.
    
    * Use NamedTemporaryFile for log file test.
    
    * fix style.
    
    * cleanup methods.
    
    * fiter websocket response for logging.
    
    * filter session request information.
    
    * filter device name from job sumission response data
    
    * update docstrings.
    
    * change param name
    
    * make substring test subTest
    
    * Fix regex and add more context
    
    * review changes
    
    * Avoid making multiple status calls least_busy
    
    * only log job submission for post requests
    
    * remove duplicate retrieve_job
    
    * filter out GET request data.
    
    * change level to getEffectiveLevel.
    
    * Refactor log filtering in session.
    
    * reuse LOG_LEVEL if provider logger not set
    
    * fix lint
    
    * Do not log request_data for certain urls.
    
    * refactor logging filtering in session.
    
    * review comments.
    
    * move log messages before object storage requests.

--- qiskit/providers/ibmq/job/ibmqjob.py
+++ qiskit/providers/ibmq/job/ibmqjob.py
@@ -271,6 +271,8 @@ class IBMQJob(BaseModel, BaseJob):
         try:
             response = self._api.job_cancel(self.job_id())
             self._cancelled = get_cancel_status(response)
+            logger.debug('Job %s cancel status is "%s". Response data: %s.',
+                         self.job_id(), self._cancelled, response)
             return self._cancelled
         except ApiError as error:
             self._cancelled = False
