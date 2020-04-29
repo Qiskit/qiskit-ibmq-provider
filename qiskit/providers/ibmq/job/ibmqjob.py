33d8e1d969a166f087a41e44f92ed3f01900975c
jessieyu <jessieyu@us.ibm.com>
2020-04-28 23:27:10 -0400

    fix lint

--- qiskit/providers/ibmq/job/ibmqjob.py
+++ qiskit/providers/ibmq/job/ibmqjob.py
@@ -29,7 +29,7 @@ from qiskit.providers.jobstatus import JOB_FINAL_STATES, JobStatus
 from qiskit.providers.models import BackendProperties
 from qiskit.qobj import QasmQobj, PulseQobj
 from qiskit.result import Result
-from qiskit.providers.ibmq import ibmqbackend
+from qiskit.providers.ibmq import ibmqbackend  # pylint: disable=unused-import
 
 from ..apiconstants import ApiJobStatus, ApiJobKind
 from ..api.clients import AccountClient
