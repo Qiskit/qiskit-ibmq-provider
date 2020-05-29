cb1ee9aab74e9527dc7838dd8c6c29434a800c3e
jessieyu <jessieyu@us.ibm.com>
2020-05-29 10:07:06 -0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 REQUIREMENTS = [
     "nest-asyncio>=1.0.0,!=1.1.0",
-    "qiskit-terra>=0.13",
+    "qiskit-terra>=0.14",
     "requests>=2.19",
     "requests-ntlm>=1.1.0",
     "websockets>=7,<8",
