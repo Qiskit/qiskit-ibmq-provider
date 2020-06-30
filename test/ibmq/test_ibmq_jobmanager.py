e1b319f3f49754008844fd22a7043af1b806ffe7
Jessie Yu <jessieyu@us.ibm.com>
2020-06-30 17:13:42 -0400

    Test with terra master (#700)
    * add terra master job
    
    * rename stage
    
    * test without cron
    
    * fix tests for terra master
    
    * revert back to cron

--- test/ibmq/test_ibmq_jobmanager.py
+++ test/ibmq/test_ibmq_jobmanager.py
@@ -422,6 +422,8 @@ class TestIBMQJobManager(IBMQTestCase):
         managed_results_methods = self._get_class_methods(ManagedResults)
         self.assertTrue(managed_results_methods)
         del managed_results_methods['combine_results']
+        for ignore_meth in ['to_dict', 'from_dict']:
+            result_methods.pop(ignore_meth, None)
 
         # Ensure both classes share the *exact* same public methods.
         self.assertEqual(result_methods.keys(), managed_results_methods.keys())
