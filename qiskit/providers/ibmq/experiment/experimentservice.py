d886ac1ea61555f9087d045779c63d22e80f9864
Jessie Yu <jessieyu@us.ibm.com>
2021-07-15 21:27:04 -0400

    fix lint

--- qiskit/providers/ibmq/experiment/experimentservice.py
+++ qiskit/providers/ibmq/experiment/experimentservice.py
@@ -479,8 +479,8 @@ class ExperimentService:
             plot: Name of the plot file or plot data to upload.
             plot_name: Name of the plot. If ``None``, the plot file name, if
                 given, or a generated name is used.
-            sync_upload: If ``True``, the plot will be uploaded synchronously. 
-                Otherwise the upload will be asynchronous. 
+            sync_upload: If ``True``, the plot will be uploaded synchronously.
+                Otherwise the upload will be asynchronous.
 
         Returns:
             A dictionary with name and size of the uploaded plot.
@@ -509,8 +509,8 @@ class ExperimentService:
             experiment: The ``Experiment`` object or the experiment UUID.
             plot: Name of the plot file or plot data to upload.
             plot_name: Name of the plot to update.
-            sync_upload: If ``True``, the plot will be uploaded synchronously. 
-                Otherwise the upload will be asynchronous. 
+            sync_upload: If ``True``, the plot will be uploaded synchronously.
+                Otherwise the upload will be asynchronous.
 
         Returns:
             A dictionary with name and size of the uploaded plot.
