13449dfe05cc52348bd38763181e8d693c9f196f
jessieyu <jessieyu@us.ibm.com>
2021-07-30 07:49:25 -0700

    apply new analysis result updates

--- test/ibmq/experiment/test_experiment_data_integration.py
+++ test/ibmq/experiment/test_experiment_data_integration.py
@@ -177,8 +177,8 @@ class TestExperimentDataIntegration(IBMQTestCase):
         """Test adding an analysis result."""
         exp_data = self._create_experiment_data()
         result_data = {"complex": 2 + 3j, "numpy": np.zeros(2)}
-        aresult = AnalysisResult(result_data=result_data,
-                                 result_type='qiskit_test',
+        aresult = AnalysisResult(name='qiskit_test',
+                                 value=result_data,
                                  device_components=self.device_components,
                                  experiment_id=exp_data.experiment_id,
                                  quality=ResultQuality.GOOD,
@@ -197,10 +197,10 @@ class TestExperimentDataIntegration(IBMQTestCase):
         aresult, exp_data = self._create_analysis_result()
 
         rdata = {"complex": 2 + 3j, "numpy": np.zeros(2)}
-        aresult.set_data(rdata)
+        aresult.value = rdata
         aresult.quality = ResultQuality.GOOD
         aresult.verified = True
-        aresult.set_tags(["foo", "bar"])
+        aresult.tags = ["foo", "bar"]
         aresult.save()
 
         rexp = DbExperimentData.load(exp_data.experiment_id, self.experiment)
@@ -210,18 +210,19 @@ class TestExperimentDataIntegration(IBMQTestCase):
     def _verify_analysis_result(self, expected, actual):
         """Verify the input analysis result."""
         self.assertEqual(expected.result_id, actual.result_id)
-        self.assertEqual(expected.result_type, actual.result_type)
+        self.assertEqual(expected.name, actual.name)
         ecomp = {str(comp) for comp in expected.device_components}
         acomp = {str(comp) for comp in actual.device_components}
         self.assertEqual(ecomp, acomp)
         self.assertEqual(expected.experiment_id, actual.experiment_id)
         self.assertEqual(expected.quality, actual.quality)
         self.assertEqual(expected.verified, actual.verified)
-        self.assertEqual(expected.tags(), actual.tags())
-        self.assertEqual(expected.data()['complex'], actual.data()['complex'])
-        self.assertEqual(expected.data()['numpy'].all(), actual.data()['numpy'].all())
-        self.assertTrue(actual.creation_datetime)
-        self.assertTrue(getattr(actual, 'creation_datetime').tzinfo)
+        self.assertEqual(expected.tags, actual.tags)
+        self.assertEqual(expected.value['complex'], actual.value['complex'])
+        self.assertEqual(expected.value['numpy'].all(), actual.value['numpy'].all())
+        # TODO: re-enable when DbAnalysisResultV1 supports kwargs again
+        # self.assertTrue(actual.creation_datetime)
+        # self.assertTrue(getattr(actual, 'creation_datetime').tzinfo)
 
     def test_delete_analysis_result(self):
         """Test deleting an analysis result."""
@@ -310,8 +311,8 @@ class TestExperimentDataIntegration(IBMQTestCase):
         """Test saving all."""
         exp_data = self._create_experiment_data()
         exp_data.set_tags(["foo", "bar"])
-        aresult = AnalysisResult(result_data={},
-                                 result_type='qiskit_test',
+        aresult = AnalysisResult(value={},
+                                 name='qiskit_test',
                                  device_components=self.device_components,
                                  experiment_id=exp_data.experiment_id)
         exp_data.add_analysis_results(aresult)
@@ -399,8 +400,8 @@ class TestExperimentDataIntegration(IBMQTestCase):
         """Test auto saving analysis result."""
         exp_data = self._create_experiment_data()
         exp_data.auto_save = True
-        aresult = AnalysisResult(result_data={},
-                                 result_type='qiskit_test',
+        aresult = AnalysisResult(value={},
+                                 name='qiskit_test',
                                  device_components=self.device_components,
                                  experiment_id=exp_data.experiment_id)
 
@@ -426,16 +427,16 @@ class TestExperimentDataIntegration(IBMQTestCase):
         aresult.auto_save = True
 
         subtests = [
-            (aresult.set_tags, (["foo"],)),
-            (aresult.set_data, ({"foo": "bar"},)),
-            (setattr, (aresult, "quality", "GOOD")),
-            (setattr, (aresult, "verified", True))
+            ("tags", ["foo"]),
+            ("value", {"foo": "bar"}),
+            ("quality", "GOOD"),
+            ("verified", True)
         ]
-        for func, params in subtests:
-            with self.subTest(func=func):
+        for attr, value in subtests:
+            with self.subTest(attr=attr):
                 with mock.patch.object(IBMExperimentService, 'update_analysis_result',
                                        wraps=exp_data.service.update_analysis_result) as mocked:
-                    func(*params)
+                    setattr(aresult, attr, value)
                     mocked.assert_called_once()
                     _, kwargs = mocked.call_args
                     self.assertEqual(aresult.result_id, kwargs['result_id'])
@@ -463,8 +464,8 @@ class TestExperimentDataIntegration(IBMQTestCase):
     def _create_analysis_result(self):
         """Create a simple analysis result."""
         exp_data = self._create_experiment_data()
-        aresult = AnalysisResult(result_data={},
-                                 result_type='qiskit_test',
+        aresult = AnalysisResult(value={},
+                                 name='qiskit_test',
                                  device_components=self.device_components,
                                  experiment_id=exp_data.experiment_id)
         exp_data.add_analysis_results(aresult)
