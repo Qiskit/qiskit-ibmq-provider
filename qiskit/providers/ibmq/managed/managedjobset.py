431b361b67be370de78e3300ebe76fb10ca2d54e
jessieyu <jessieyu@us.ibm.com>
2021-10-01 10:50:29 -0400

    use function

--- qiskit/providers/ibmq/managed/managedjobset.py
+++ qiskit/providers/ibmq/managed/managedjobset.py
@@ -396,7 +396,7 @@ class ManagedJobSet:
             if isinstance(experiment, (QuantumCircuit, Schedule)):
                 experiment = experiment.name
             for job in self.jobs():
-                for i, exp in enumerate(job._qobj.experiments):
+                for i, exp in enumerate(job._get_qobj().experiments):
                     if hasattr(exp.header, 'name') and exp.header.name == experiment:
                         return job, i
 
