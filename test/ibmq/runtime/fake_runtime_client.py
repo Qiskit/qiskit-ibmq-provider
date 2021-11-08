582ba052bc704cb15387b04d230a657d87ff8ae1
Rathish Cholarajan <rathishc24@gmail.com>
2021-11-08 07:44:37 -0500

    Support pagination for retrieving runtime programs (#1087)

--- test/ibmq/runtime/fake_runtime_client.py
+++ test/ibmq/runtime/fake_runtime_client.py
@@ -232,12 +232,12 @@ class BaseFakeRuntimeClient:
         """Set job status to passed in final status instantly."""
         self._final_status = final_status
 
-    def list_programs(self):
+    def list_programs(self, limit, skip):
         """List all programs."""
         programs = []
         for prog in self._programs.values():
             programs.append(prog.to_dict())
-        return {"programs": programs}
+        return {"programs": programs[skip:limit+skip], "count": len(self._programs)}
 
     def program_create(self, program_data, name, description, max_execution_time,
                        spec=None, is_public=False):
