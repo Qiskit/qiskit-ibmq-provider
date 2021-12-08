6ceac60301581c885579719862ab923ee09e6070
Jessie Yu <jessieyu@us.ibm.com>
2021-12-08 10:37:49 -0500

    Remove extra decompose in RuntimeEncoder (#1094)
    * remove extra decompose
    
    * fix lint
    
    * fix lint again
    
    * add release note

--- qiskit/providers/ibmq/runtime/utils.py
+++ qiskit/providers/ibmq/runtime/utils.py
@@ -36,7 +36,6 @@ except ImportError:
 
 from qiskit.circuit import (Instruction, ParameterExpression, QuantumCircuit,
                             qpy_serialization)
-from qiskit.circuit.library import BlueprintCircuit
 from qiskit.result import Result
 
 
@@ -184,9 +183,6 @@ class RuntimeEncoder(json.JSONEncoder):
         if hasattr(obj, 'to_json'):
             return {'__type__': 'to_json', '__value__': obj.to_json()}
         if isinstance(obj, QuantumCircuit):
-            # TODO Remove the decompose when terra 6713 is released.
-            if isinstance(obj, BlueprintCircuit):
-                obj = obj.decompose()
             value = _serialize_and_encode(
                 data=obj,
                 serializer=lambda buff, data: qpy_serialization.dump(data, buff)
