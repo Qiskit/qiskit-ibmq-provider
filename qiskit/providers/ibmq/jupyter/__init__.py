dd0524b5be5f3457b86560576794406e5cb7371f
kevin-tian <kevin.tian@ibm.com>
2023-01-14 17:09:07 -0500

    remove jupyter imports

--- qiskit/providers/ibmq/jupyter/__init__.py
+++ qiskit/providers/ibmq/jupyter/__init__.py
@@ -67,20 +67,3 @@ IBM Quantum Experience (IQX) dashboard
     %iqx_dashboard
 
 """
-import sys
-
-if ('ipykernel' in sys.modules):
-
-    from IPython import get_ipython          # pylint: disable=import-error
-    from .dashboard.dashboard import IQXDashboardMagic
-    from qiskit.providers.fake_provider.fake_backend import FakeBackend
-    from ..ibmqbackend import IBMQBackend
-    from .backend_info import backend_widget
-
-    _IP = get_ipython()
-    if _IP is not None:
-        _IP.register_magics(IQXDashboardMagic)
-        HTML_FORMATTER = _IP.display_formatter.formatters['text/html']
-        # Make backend_widget the html repr for IBM Quantum backends
-        HTML_FORMATTER.for_type(IBMQBackend, backend_widget)
-        HTML_FORMATTER.for_type(FakeBackend, backend_widget)
